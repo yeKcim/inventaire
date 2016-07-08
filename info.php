9430552af97a8147515d1c84f720818f78a6f852
CARRÉ Anthony <anthony.carre@univ-rennes1.fr>
2016-07-08 14:31:07 +0200

    cosmetics

--- info.php
+++ info.php
@@ -12,20 +12,28 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta name="robots" content="noindex,nofollow" />
     <title>Informations détaillées</title>
-    <link rel="stylesheet" href="chosen/chosen.css">
     <link rel="stylesheet" type="text/css" href="./style.css">
     
     <!-- ascii : http://patorjk.com/software/taag/#p=display&h=2&v=0&f=ANSI%20Shadow&t= -->
     
-    <!-- tinybox pour les popup layout -->
+<!--╔╦╗╦╔╗╔╦ ╦╔╗ ╔═╗═╗ ╦
+     ║ ║║║║╚╦╝╠╩╗║ ║╔╩╦╝
+     ╩ ╩╝╚╝ ╩ ╚═╝╚═╝╩ ╚═
+     tinybox pour les popup layout -->
     <script type="text/javascript" src="tinybox/tinybox.js"></script>
     <link rel="stylesheet" href="tinybox/tinybox.css" />
     
-    <!-- dropzone pour drag&drop uploader
+<!--╔╦╗╦═╗╔═╗╔═╗╔═╗╔═╗╔╗╔╔═╗
+     ║║╠╦╝║ ║╠═╝╔═╝║ ║║║║║╣ 
+    ═╩╝╩╚═╚═╝╩  ╚═╝╚═╝╝╚╝╚═╝
+    dropzone pour drag&drop uploader    -->
     <script src="dropzone/dropzone.js"></script>
-    <link rel="stylesheet" href="dropzone/dropzone.css">    -->
+    <link rel="stylesheet" href="dropzone/dropzone.css">
     
-    <!-- Show/Hide form -->
+
+<!--╔╦╗╦╔═╗╔═╗╦  ╔═╗╦ ╦   ╦ ╦╦╔╦╗╔═╗
+     ║║║╚═╗╠═╝║  ╠═╣╚╦╝ █ ╠═╣║ ║║║╣ 
+    ═╩╝╩╚═╝╩  ╩═╝╩ ╩ ╩    ╩ ╩╩═╩╝╚═╝    -->
     <script type="text/javascript">
         // <![CDATA[
         function display(obj,id1,id2) {
@@ -49,7 +57,13 @@
         }
         // ]]>
     </script>
-
+    
+<!--╔═╗╦ ╦╔═╗╔═╗╔═╗╔═╗╔╗╔     ╔╦╗╦ ╦╦ ╔╦╗╦  ╔═╗╔═╗╦  ╔═╗╔═╗╔╦╗
+    ║  ╠═╣║ ║║ ║╚═╗║╣ ║║║  o  ║║║║ ║║  ║ ║  ╚═╗║╣ ║  ║╣ ║   ║ 
+    ╚═╝╩ ╩╚═╝╚═╝╚═╝╚═╝╝╚╝  o  ╩ ╩╚═╝╩═╝╩ ╩  ╚═╝╚═╝╩═╝╚═╝╚═╝ ╩   -->
+    <script src="chosen/jquery.min.js" type="text/javascript"></script>
+    <script src="chosen/chosen.jquery.js" type="text/javascript"></script>
+    <link rel="stylesheet" href="chosen/chosen.css">
 
 </head>
 
@@ -70,10 +84,7 @@ require_once("./tables_sql_commun.php");
 $i= isset($_GET["i"]) ? htmlentities($_GET["i"]) : "" ; // GET i
 require_once("./fonctions.php");
 
-/* Intégration de choosen*/
-echo "<script src=\"chosen/jquery.min.js\" type=\"text/javascript\"></script>";
-echo "<script src=\"chosen/chosen.jquery.js\" type=\"text/javascript\"></script>";
-  
+
 
 /*
  █████╗ ██████╗ ██████╗  █████╗ ██╗   ██╗
@@ -92,7 +103,7 @@ while ($l = mysql_fetch_row($query_table)) {
         "base_index"=>$l[0],                  "lab_id"=>$l[1],                  "categorie"=>$l[2],
         "serial_number"=>utf8_encode($l[3]),  "reference"=>utf8_encode($l[4]),  "designation"=>$l[5],
         "utilisateur"=>$l[6],                 "localisation"=>$l[7],            "date_localisation"=>$l[8],
-        "tutelle"=>$l[9],                     "contrat"=>$l[10],                "num_inventaire"=>utf8_encode($l[11]),        
+        "tutelle"=>$l[9],                     "contrat"=>$l[10],                "num_inventaire"=>utf8_encode($l[11]),
         "vendeur"=>$l[12],                    "marque"=>$l[13],                 "date_achat"=>$l[14],
         "responsable_achat"=>$l[15],          "garantie"=>$l[16],               "prix"=>$l[17],
         "date_sortie"=>$l[18],                "sortie"=>$l[19],                 "raison_sortie"=>$l[20],
