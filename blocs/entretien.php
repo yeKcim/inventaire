94cbccab1d84984004211350e11745043419043f
CARRÉ Anthony <anthony.carre@univ-rennes1.fr>
2016-07-13 10:39:05 +0200

    nouvel entretien fonctionnel, version α de l’affichage des entretiens

--- blocs/entretien.php
+++ blocs/entretien.php
@@ -11,18 +11,66 @@
 
 
 
+/*
+███╗   ███╗ ██████╗ ██████╗ ██╗███████╗    ███████╗ ██████╗ ██╗     
+████╗ ████║██╔═══██╗██╔══██╗██║██╔════╝    ██╔════╝██╔═══██╗██║     
+██╔████╔██║██║   ██║██║  ██║██║█████╗      ███████╗██║   ██║██║     
+██║╚██╔╝██║██║   ██║██║  ██║██║██╔══╝      ╚════██║██║▄▄ ██║██║     
+██║ ╚═╝ ██║╚██████╔╝██████╔╝██║██║██╗      ███████║╚██████╔╝███████╗
+╚═╝     ╚═╝ ╚═════╝ ╚═════╝ ╚═╝╚═╝╚═╝      ╚══════╝ ╚══▀▀═╝ ╚══════╝
+*/
+
+/*  ╔═╗ ╦╔═╗╦ ╦╔╦╗  ╔═╗╔╗╔╦╗╦═╗╔═╗╔╦╗╦╔═╗╔╗╔
+    ╠═╣ ║║ ║║ ║ ║   ║╣ ║║║║ ╠╦╝║╣  ║ ║║╣ ║║║
+    ╩ ╩╚╝╚═╝╚═╝ ╩   ╚═╝╝╚╝╩ ╩╚═╚═╝ ╩ ╩╚═╝╝╚╝    */
+if ( isset($_POST["add_entretien"]) ) {
+    $arr = array("e_frequence", "e_frequence_multipli", "e_designation", "e_detail");
+    foreach ($arr as &$value) {
+        $$value= isset($_POST[$value]) ? htmlentities($_POST[$value]) : "" ;
+    }
+    $e_frequence=$e_frequence*$e_frequence_multipli;
+    mysql_query ("INSERT INTO entretien (e_id, e_frequence, e_lastdate, e_designation, e_detail) VALUES ($i,\"$e_frequence\", \"".date("Y-m-d")."\", \"".$e_designation."\", \"".$e_detail."\"); ");
+}
+
+/*  ╔╦╗╔═╗╔╦╗╦╔═╗  ╔═╗╔╗╔╦╗╦═╗╔═╗╔╦╗╦╔═╗╔╗╔
+    ║║║║ ║ ║║║╠╣   ║╣ ║║║║ ╠╦╝║╣  ║ ║║╣ ║║║
+    ╩ ╩╚═╝═╩╝╩╚    ╚═╝╝╚╝╩ ╩╚═╚═╝ ╩ ╩╚═╝╝╚╝    */
+if ($modif_entretien=="Entretien effectué") {
+    
+    
+    
+    
+}
+
+
+/* TODO
+    if ($utilisateur=="plus_utilisateur") {
+        $plus_utilisateur_nom=mb_strtoupper($plus_utilisateur_nom);
+        $plus_utilisateur_phone=phone_display("$plus_utilisateur_phone","");
+        mysql_query ("INSERT INTO utilisateur (utilisateur_nom, utilisateur_prenom, utilisateur_mail, utilisateur_phone) VALUES ('".$plus_utilisateur_nom."', '".$plus_utilisateur_prenom."','".$plus_utilisateur_mail."','".$plus_utilisateur_phone."') ; ");
+        // TODO : prévoir le cas où le contrat existe déjà
+        $query_table_utilisateurnew = mysql_query ("SELECT utilisateur_index FROM utilisateur ORDER BY utilisateur_index DESC LIMIT 1 ;");
+        while ($l = mysql_fetch_row($query_table_utilisateurnew)) $utilisateur=$l[0];
+        // on ajoute cette entrée dans le tableau des utilisateurs (utilisé pour le select)
+        $utilisateurs[$utilisateur]=array( $utilisateur, utf8_encode($plus_utilisateur_nom), utf8_encode($plus_utilisateur_prenom), utf8_encode($plus_utilisateur_mail), phone_display("$plus_utilisateur_phone",".") );
+    }
+*/
 
 
 
-
-
-
-
-
-
-
-
-
+/*
+ █████╗ ██████╗ ██████╗  █████╗ ██╗   ██╗
+██╔══██╗██╔══██╗██╔══██╗██╔══██╗╚██╗ ██╔╝
+███████║██████╔╝██████╔╝███████║ ╚████╔╝ 
+██╔══██║██╔══██╗██╔══██╗██╔══██║  ╚██╔╝  
+██║  ██║██║  ██║██║  ██║██║  ██║   ██║   
+╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   
+*/
+$query_table_entretien = mysql_query ("SELECT e_index, e_frequence, e_lastdate, e_designation, e_detail, e_effectuerpar FROM entretien WHERE e_id=$i ORDER BY e_designation ASC ;");
+$entretiens = array();
+while ($l = mysql_fetch_row($query_table_entretien)) {
+    $entretiens[]=array($l[0], $l[1], $l[2], utf8_encode($l[3]), utf8_encode($l[4]), $l[5]);
+}
 
 
 /*
@@ -33,7 +81,7 @@
 ██║     ╚██████╔╝██║  ██║██║ ╚═╝ ██║╚██████╔╝███████╗██║  ██║██║██║  ██║███████╗
 ╚═╝      ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝╚═╝╚═╝  ╚═╝╚══════╝
 */
-echo "<div id=\"bloc\" style=\"background:#d299a3; vertical-align:top;\">";
+echo "<div id=\"bloc\" style=\"background:#f998a9; vertical-align:top;\">";
 
     echo "<h1>Entretien</h1>";
     
@@ -53,7 +101,7 @@ echo "<div id=\"bloc\" style=\"background:#d299a3; vertical-align:top;\">";
         echo "<select name=\"e_frequence_multipli\" id=\"e_frequence_multipli\">";
         echo "<option value=\"1\">jours</option>";
         echo "<option value=\"30\" selected>mois</option>"; 
-        echo "<option value=\"365\">Non</option>";
+        echo "<option value=\"365\">ans</option>";
         echo "</select> <br/>";
 
             /* ########### Désignation ########### */
@@ -76,17 +124,17 @@ echo "<div id=\"bloc\" style=\"background:#d299a3; vertical-align:top;\">";
 
     echo "<fieldset><legend>Entretiens</legend>";
 
-    $entretien=array(0,0);
-
         echo "<table style=\"border:none;\">";
-        foreach ($entretien as $e) {
+        
+        foreach ($entretiens as $e) {
             echo "<tr>";
             echo "<td><input type=\"checkbox\" id=\"cbox1\" value=\"1\"></td>";
-            echo "<td>designation un peu longue que ce passe-t-il ? (détails)</td>";
+            echo "<td>".$e[0]."+".$e[1]."+".$e[2]."+".$e[3]."+".$e[4]."+".$e[5]."</td>";
             echo "<td>date (retard ou reste)</td>";
             echo "<td style=\"text-align:right;\"><span id=\"linkbox\" onclick=\"TINY.box.show({url:'del_confirm.php?i=$i&e=\$e[0]',width:280,height:110})\" title=\"cet entretien n’est plus nécessaire\">×<span></td>";
             echo "</tr>";
         }
+
         echo "</table>";
 
         /* ########### Le ########### */
@@ -94,8 +142,32 @@ echo "<div id=\"bloc\" style=\"background:#d299a3; vertical-align:top;\">";
         echo "<input value=\"\" name=\"e_effectuele\" type=\"text\" id=\"e_effectuele\"><br/>";
 
         /* ########### Par ########### */
-        echo "<label for=\"e_effectuepar\" style=\"vertical-align: top;\"> par :</label>\n";
-        echo "<input value=\"\" name=\"e_effectuepar\" type=\"text\" id=\"e_effectuepar\"><br/>";
+        echo "<label for=\"e_effectuepar\">Par : </label>\n";
+        echo "<select name=\"e_effectuepar\" onchange=\"display(this,'plus_intervant','plus_intervant');\" id=\"e_effectuepar\">";
+        echo "<option value=\"0\" "; if ($entretiens[5]=="0") echo "selected"; echo ">— Aucun intervenant spécifié —</option>"; 
+        option_selecteur($entretiens[5], $utilisateurs, "2");
+        echo "<option value=\"plus_intervant\" "; if ($entretiens[5]=="plus_intervant") echo "selected"; echo ">Nouvel intervenant :</option>";
+        echo "</select><br/>";
+
+            /* ########### + utilisateur ########### */
+            echo "\n\n\n";
+            echo "<fieldset id=\"plus_intervant\" class=\"subfield\" style=\"display: none;\"><legend class=\"subfield\">Nouvel intervant</legend>";
+                echo "<label for=\"plus_intervant_prenom\">Prénom :</label>\n";
+                echo "<input value=\"\" name=\"plus_intervant_prenom\" type=\"text\">\n";
+
+                echo "<label for=\"plus_intervant_nom\">Nom :</label>\n";
+                echo "<input value=\"\" name=\"plus_intervant_nom\" type=\"text\">\n";
+                
+                echo "<label for=\"plus_intervant_mail\">Mail :</label>\n";
+                echo "<input value=\"\" name=\"plus_intervant_mail\" type=\"text\">\n";
+                
+                echo "<label for=\"plus_intervant_phone\">Téléphone :</label>\n";
+                echo "<input value=\"\" name=\"plus_intervant_phone\" type=\"text\">\n";
+
+            echo "</fieldset>";
+            echo "\n\n\n";
+
+
 
         /* ########### submit ########### */
         echo "<input name=\"modif_entretien\" value=\"Entretien effectué\" type=\"submit\">";
