b834b0b9d41eef6f21716eb243084052f51d7059
CARRÉ Anthony <anthony.carre@univ-rennes1.fr>
2016-07-19 12:23:42 +0200

    head dans un fichier séparé

--- index.php
+++ index.php
@@ -1,16 +1,4 @@
-<!-- ########### HEAD ########### -->
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html lang="fr" dir="ltr" xml:lang="fr" xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <meta name="robots" content="noindex,nofollow" />
-    <title>Base de données OPTIQUE</title>
-    <link rel="stylesheet" type="text/css" href="./style.css">
-    
-    <link rel="stylesheet" href="tinybox/tinybox.css" />
-    <script type="text/javascript" src="tinybox/tinybox.js"></script>
-
-</head>
+<?php require_once("./head.html"); ?>
 
 <!-- ########### BODY ########### -->
 <body>
