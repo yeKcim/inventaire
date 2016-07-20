2a9ae51c5b8de7ca1966a717a50eafc1690949f8
CARRÉ Anthony <anthony.carre@univ-rennes1.fr>
2016-07-20 11:36:28 +0200

    quelques améliorations, toujours pas fonctionnel

--- add.php
+++ add.php
@@ -21,6 +21,7 @@ while ($l = mysql_fetch_row($inew)) $i=$l[0]+1;
 
 require_once("./fonctions.php");
 
+$error="";
 
 /*
  █████╗ ██████╗ ██████╗  █████╗ ██╗   ██╗
@@ -33,10 +34,60 @@ require_once("./fonctions.php");
 
 /* ########### INFORMATIONS COMPOSANT ########### */
 if ( isset($_POST["add_valid"]) ) {
-    $arr = array("categorie", "plus_categorie_nom", "plus_categorie_abbr", "marque", "plus_marque", "plus_marque_nom", "reference", "serial_number", "plus_tags", "designation", "vendeur", "plus_vendeur_nom", "plus_vendeur_web", "plus_vendeur_remarque", "prix", "contrat", "plus_contrat_nom", "contrat_type", "plus_contrat_type_nom", "tutelle", "plus_tutelle", "num_inventaire", "responsable_achat", "plus_responsable_achat_prenom", "plus_responsable_achat_nom", "plus_responsable_achat_mail", "plus_responsable_achat_phone", "date_achat", "garantie", "utilisateur", "plus_utilisateur_prenom", "plus_utilisateur_nom", "plus_utilisateur_mail", "plus_utilisateur_phone", "localisation", "plus_localisation_bat", "plus_localisation_piece", "sortie", "raison_sortie", "plus_raison_sortie_nom", "integration");
+    $data=array();
+    $arr = array("categorie", "plus_categorie_nom", "plus_categorie_abbr", "marque", "plus_marque_nom", "reference", "serial_number", "plus_tags", "designation", "vendeur", "plus_vendeur_nom", "plus_vendeur_web", "plus_vendeur_remarque", "prix", "contrat", "plus_contrat_nom", "contrat_type", "plus_contrat_type_nom", "tutelle", "num_inventaire", "responsable_achat", "plus_responsable_achat_prenom", "plus_responsable_achat_nom", "plus_responsable_achat_mail", "plus_responsable_achat_phone", "date_achat", "garantie", "utilisateur", "plus_utilisateur_prenom", "plus_utilisateur_nom", "plus_utilisateur_mail", "plus_utilisateur_phone", "localisation", "plus_localisation_bat", "plus_localisation_piece", "sortie", "raison_sortie", "plus_raison_sortie_nom", "integration");
     foreach ($arr as &$value) {
-        $$value= isset($_POST[$value]) ? htmlentities($_POST[$value]) : "" ;
+        $data["$value"]= isset($_POST[$value]) ? htmlentities($_POST[$value]) : "" ;
     }
+
+    
+    if ( ($data["categorie"]=="0")&&($data["designation"]=="") ) $error.="<p class=\"error_message\">Merci de remplir au minimum Administratif→Désignation ou Technique→Catégorie</p>";
+    
+    
+    
+    /*
+    if ($data["categorie"]=="plus_categorie")
+        "plus_categorie_nom"
+        "plus_categorie_abbr"
+    if ($data["marque"]=="plus_marque")
+        "plus_marque_nom"
+    "plus_tags"
+    tags[] ????
+    compatibilite[] ????
+    if ($data["vendeur"]=="plus_vendeur")
+        "plus_vendeur_nom"
+        "plus_vendeur_web"
+        "plus_vendeur_remarque"
+    if ($data["contrat"]=="plus_contrat")
+        "plus_contrat_nom"
+        if ($data["contrat_type"]=="plus_contrat_type")
+            "plus_contrat_type_nom"
+    if ($data["tutelle"]=="plus_tutelle")
+    if ($data["responsable_achat"]=="plus_responsable_achat")
+        "plus_responsable_achat_prenom"
+        "plus_responsable_achat_nom"
+        "plus_responsable_achat_mail"
+        "plus_responsable_achat_phone"
+        */
+    $datamysql=array();
+    
+    $datamysql["reference"]=$data["reference"];
+    $datamysql["serial_number"]=$data["serial_number"];
+    $datamysql["prix"]=$data["prix"];
+    $datamysql["num_inventaire"]=$data["num_inventaire"];
+    $datamysql["serial_number"]=$data["serial_number"];
+    $datamysql["designation"]=$data["designation"];
+    $datamysql["date_achat"]=($data["date_achat"]=="") ? "0000-00-00" : dateformat($data["date_achat"],"en");
+    $datamysql["garantie"]=($data["garantie"]=="") ? "0000-00-00" : dateformat($data["garantie"],"en");
+    $datamysql["base_index"]=$i;
+    $datamysql["base_index"]=$i;
+    $datamysql["lab_id"] = new_lab_id($data["categorie"]);
+    
+    $mysql="INSERT
+        INTO base (base_index, lab_id, categorie, serial_number, reference, designation, utilisateur, localisation, date_localisation, tutelle, contrat, num_inventaire, vendeur, marque, date_achat, responsable_achat, garantie, prix, date_sortie, sortie, raison_sortie, integration)
+        VALUES ('".$i."', '".$datamysql["lab_id"]."', '***categorie***', '".$datamysql["serial_number"]."', '".$datamysql["reference"]."', '".$datamysql["designation"]."', '0', '0', '0000-00-00', '***tutelle***', '***contrat***', '".$datamysql["num_inventaire"]."', '***vendeur***', '***marque***', '".$datamysql["date_achat"]."', '***responsable_achat***', '".$datamysql["garantie"]."', '".$datamysql["prix"]."', '0000-00-00', '0', '0', '0'); ";
+    
+    
 }
 else { // Initialisation de toutes les variable
     $data=array(
@@ -73,15 +124,30 @@ echo "<form method=\"post\" action=\"?a=$i\">";
 echo "<div id=\"container\">";
     require_once("./blocs/administratif.php");
     require_once("./blocs/technique.php");
-    require_once("./blocs/utilisation.php");
+
+
+    /*  ╔═╗╦ ╦╔╗ ╔╦╗╦╔╦╗
+        ╚═╗║ ║╠╩╗║║║║ ║ 
+        ╚═╝╚═╝╚═╝╩ ╩╩ ╩     */
+    echo "<div id=\"bloc\" style=\"background:#f3f3f3; vertical-align:top;\">";
+    echo "<h1>Validation</h1>";
+    echo "<p style=\"text-align:center;\">";
+    echo "<input name=\"add_valid\" value=\"Ajouter\" type=\"submit\" style=\"padding: 10px 50px;\">";
+    echo "</p>"; // TODO Ajouter un bouton réinitialiser
+    
+    echo $error;
+
+
+
+    echo $mysql;
+
+
+
+    echo "</div>";
+
 echo "</div>";
 
-/*  ╔═╗╦ ╦╔╗ ╔╦╗╦╔╦╗
-    ╚═╗║ ║╠╩╗║║║║ ║ 
-    ╚═╝╚═╝╚═╝╩ ╩╩ ╩     */
-echo "<p style=\"text-align:center;\"><input name='add_valid' value='Ajouter' type='submit'></p>"; // TODO Ajouter un bouton réinitialiser
-    
-
+echo "</form>"
 
 ?>
 
