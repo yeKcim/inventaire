e0f90229f61802407e4080b2c4f3059f786c732d
CARRÉ Anthony <anthony.carre@univ-rennes1.fr>
2016-10-28 15:50:08 +0200

    allow more extensions

--- 0_config.php
+++ 0_config.php
@@ -7,7 +7,7 @@ $connectpasse="Mais-Ou#Est+Donc_0r!Ni42Car?";
 $database="optique";
 
 
-$extensions= array("pdf","jpg","png","svg","txt");    // liste des extensions autorisées pour l’envoi de fichiers
+$extensions= array("pdf","jpg","png","svg","txt", "gif", "dxf", "sldprt", "step");    // liste des extensions autorisées pour l’envoi de fichiers
 $racine="/var/www/";                            // dossier où est installé « Inventaire »
 $dossierdesfichiers="".$racine."files/";        // dossier où sont placés les fichiers (doit être 775)
 $trash="".$dossierdesfichiers."trash/";         // dossier corbeille des fichiers
