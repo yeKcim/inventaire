11010b9ce109f1c7e2f282dfb9901b6602099c32
yeKcim <yeKcim@users.noreply.github.com>
2017-10-19 15:17:14 +0200

    Update 0_config.php

--- 0_config.php
+++ 0_config.php
@@ -4,7 +4,7 @@ $connecthost="localhost";
 $connectlogin="<YOUR_LOGIN>";
 $connectpasse="<YOUR_PASSWORD>";
 
-$database="optique";
+$database="<YOUR_DATABASE>";
 
 $extensions= array("pdf","jpg","png","svg","txt", "gif", "dxf", "sldprt", "step", "zip", "7z");    // liste des extensions autorisées pour l’envoi de fichiers
 $racine="/var/www/";                            // dossier où est installé « Inventaire »
