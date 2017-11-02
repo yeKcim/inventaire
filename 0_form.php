c28a8e8c7e63ac1ec128ad9bb51aee37017c40fe
yeKcim <yekcim@crocobox.org>
2017-11-02 15:28:53 +0100

    cosmetic

--- 0_form.php
+++ 0_form.php
@@ -70,7 +70,7 @@ $UTL_CMD= ($UTL!="") ? "AND utilisateur=$UTL" : "" ;
 #########################################################################
 #                           SEA : Recherche                             #
 #########################################################################
-echo "<br/>";
+echo " ";
 echo "  <input value=\"$SEA_textbox\" name=\"SEA\" type=\"text\" onFocus=\"if (this.value=='$SEA_textbox') {this.value=''}\">
 	    <input value=\"Chercher\" type=\"submit\">";
 $SEA_CMD= ($SEA!="") ? "AND (lab_id LIKE '%$SEA%' OR categorie LIKE '%$SEA%' OR reference LIKE '%$SEA%' OR designation LIKE '%$SEA%' OR marque_nom LIKE '%$SEA%' OR vendeur_nom LIKE '%$SEA%' OR serial_number LIKE '%$SEA%' OR num_inventaire LIKE '%$SEA%' OR bon_commande LIKE '%$SEA%')" : "";
