0051d117c689ad7d335d59ea9b8e8b8f8237e381
yeKcim <yeKcim@users.noreply.github.com>
2019-02-04 15:08:12 +0100

    Update 0_config.php
    database is define out this file

--- 0_config.php
+++ 0_config.php
@@ -4,14 +4,9 @@ $connecthost="localhost";
 $connectlogin="<YOUR_LOGIN>";
 $connectpasse="<YOUR_PASSWORD>";
 
-$database="<YOUR_DATABASE>";
-
 $extensions= array("pdf","jpg","png","svg","txt", "gif", "dxf", "sldprt", "step", "zip", "7z");    // liste des extensions autorisées pour l’envoi de fichiers
 $racine="/var/www/";                            // dossier où est installé « Inventaire »
 $dossierdesfichiers="".$racine."files/";        // dossier où sont placés les fichiers (doit être 775)
 $trash="".$dossierdesfichiers."trash/";         // dossier corbeille des fichiers
-
-
-
 
 ?>
