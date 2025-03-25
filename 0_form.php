5c8b1338dc72588c0faf4889cd4d6c93f17b84fb
yeKcim <yekcim@crocobox.org>
2025-03-25 15:51:14 +0100

    chosen → select2

--- 0_form.php
+++ 0_form.php
@@ -10,19 +10,25 @@ echo "<form method=\"get\" action=\"?\">";
 #########################################################################
 #                        IOT : in/out/tmp                               #
 #########################################################################
-//selecteur_chosen("IOT", $iot, "— Répertorié (sauf sorti déf.) —", "iot_index", "iot_nom");
+//selecteur_selectwo("IOT", $iot, "— Répertorié (sauf sorti déf.) —", "iot_index", "iot_nom");
 $iot=array();
 $iot[0]=array("iot_index"=> "0,2", "iot_nom" => "— Inventaire —");
 $iot[1]=array("iot_index"=> "0", "iot_nom" => "Uniquement non sorti");
 $iot[2]=array("iot_index"=> "2", "iot_nom" => "Uniquement sorti temp (prêt, réparation,…)");
 $iot[3]=array("iot_index"=> "1", "iot_nom" => "Uniquement sorti définitivement");
-echo "<select name=\"IOT\" onchange=\"submit();\" data-placeholder=\"Choose…\" class=\"chosen-select\" tabindex=\"0\">";
+echo "<select name=\"IOT\" onchange=\"submit();\" data-placeholder=\"Choose…\" class=\"select2\" tabindex=\"0\" id=\"iot\">";
 foreach ($iot as $i) {
 	echo "<option value=\"".$i["iot_index"]."\" ";
 	if ($IOT==$i["iot_index"]) echo "selected";
 	echo ">".$i["iot_nom"]."</option>";
 }
 echo "</select> ";
+/*select2 pour recherche */
+        echo "<script>
+			$(document).ready(function() {
+				$('#iot').select2({minimumResultsForSearch: Infinity});
+			});
+		</script>";
 
 //$IOT= ($IOT=="") ? "1,2" : $IOT ;
 $IOT_CMD= ($IOT!="") ? "AND sortie IN ($IOT)" : "AND sortie IN (0,2)" ;
@@ -31,7 +37,7 @@ $IOT_CMD= ($IOT!="") ? "AND sortie IN ($IOT)" : "AND sortie IN (0,2)" ;
 #                        CAT : categorie                                #
 #########################################################################
 // $categories in tables_sql_commun.php
-selecteur_chosen("CAT", $categories, "Toutes catégories", "categorie_index", "categorie_nom", "categorie_lettres", "display()");
+selecteur_selectwo("CAT", $categories, "Toutes catégories", "categorie_index", "categorie_nom", "categorie_lettres", "display()");
 $CAT_CMD= ($CAT!="") ? "AND categorie_index IN ($CAT)" : "" ;
 
 
@@ -51,7 +57,7 @@ echo "<br/>";
 #                        TYC : type de contrat                          #
 #########################################################################
 // $types_contrats in tables_sql_commun.php
-selecteur_chosen("TYC", $types_contrats, "Tous types de contrat", "contrat_type_index", "contrat_type_cat");
+selecteur_selectwo("TYC", $types_contrats, "Tous types de contrat", "contrat_type_index", "contrat_type_cat");
 $TYC_CMD= ($TYC!="") ? "AND contrat_type_index=$TYC" : "" ;
 
 #########################################################################
@@ -60,7 +66,7 @@ $TYC_CMD= ($TYC!="") ? "AND contrat_type_index=$TYC" : "" ;
 $LIM_CON= ($TYC!="") ? "AND contrat_type=\"$TYC\" " : "" ; // si type de contrat sélectionné, formulaire contrats se limite
 $sth = $dbh->query("SELECT contrat_index, contrat_nom, contrat_type FROM contrat, contrat_type WHERE contrat_index!=0 AND contrat_type_index!=0 AND contrat_type=contrat_type.contrat_type_index $LIM_CON ORDER BY contrat_nom ASC ;");
 $contrats = ($sth) ? $sth->fetchAll(PDO::FETCH_ASSOC) : FALSE ;
-selecteur_chosen("CON", $contrats, "Tous contrats", "contrat_index", "contrat_nom");
+selecteur_selectwo("CON", $contrats, "Tous contrats", "contrat_index", "contrat_nom");
 $CON_CMD= ($CON!="") ? "AND contrat_index=$CON" : "" ;
 if ($sth) $sth->closeCursor();
 
@@ -69,7 +75,7 @@ if ($sth) $sth->closeCursor();
 #########################################################################
 $sth = $dbh->query("SELECT DISTINCT (utilisateur_index) responsable_achat, utilisateur_nom, utilisateur_prenom, utilisateur_mail, utilisateur_phone FROM utilisateur, base WHERE responsable_achat=utilisateur_index AND utilisateur_index!=0 ORDER BY utilisateur_nom, utilisateur_prenom ASC ;");
 $responsables = ($sth) ? $sth->fetchAll(PDO::FETCH_ASSOC) : FALSE ;
-selecteur_chosen("RES", $responsables, "Tous responsables achat", "responsable_achat", "utilisateur_nom", "utilisateur_prenom");
+selecteur_selectwo("RES", $responsables, "Tous responsables achat", "responsable_achat", "utilisateur_nom", "utilisateur_prenom");
 $RES_CMD= ($RES!="") ? "AND responsable_achat=$RES" : "" ;
 if ($sth) $sth->closeCursor();
 
@@ -78,7 +84,7 @@ if ($sth) $sth->closeCursor();
 #########################################################################
 $sth = $dbh->query("SELECT DISTINCT(utilisateur_index) utilisateur_index, utilisateur_nom, utilisateur_prenom, utilisateur_mail, utilisateur_phone FROM utilisateur, base WHERE utilisateur=utilisateur_index AND utilisateur_index!=0 ORDER BY utilisateur_nom ASC ;");
 $utilisateurs = ($sth) ? $sth->fetchAll(PDO::FETCH_ASSOC) : FALSE ;
-selecteur_chosen("UTL", $utilisateurs, "Tous les utilisateurs", "utilisateur_index", "utilisateur_nom", "utilisateur_prenom");
+selecteur_selectwo("UTL", $utilisateurs, "Tous les utilisateurs", "utilisateur_index", "utilisateur_nom", "utilisateur_prenom");
 $UTL_CMD= ($UTL!="") ? "AND utilisateur=$UTL" : "" ;
 if ($sth) $sth->closeCursor();
 
