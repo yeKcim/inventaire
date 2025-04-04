f3a8d3e4d6fab87e9db107e4ba2063252da288e9
yeKcim <yekcim@crocobox.org>
2025-04-04 12:09:33 +0200

    ajout désignation dans description "est intégré dans"

--- blocs/utilisation.php
+++ blocs/utilisation.php
@@ -476,7 +476,7 @@ echo "<div id=\"bloc\" style=\"background:#c3d1e1; vertical-align:top;\">";
         echo "<select name=\"integration\" id=\"integration\" >";
        
         echo "<option value=\"0\" "; if ($data[0]["integration"]=="0") echo "selected"; echo ">— Aucune intégration spécifiée —</option>";
-        option_selecteur($data[0]["integration"], $lab_ids, "base_index", "lab_id");
+        option_selecteur($data[0]["integration"], $lab_ids, "base_index", "lab_id", "designation");
         echo "</select>";
 		echo "<script>
 			\$j(document).ready(function() {
