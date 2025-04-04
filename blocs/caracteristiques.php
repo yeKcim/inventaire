56685ed93fa5c487ad99d6bb4a21f83751bc29a7
yeKcim <yekcim@crocobox.org>
2025-04-04 16:18:40 +0200

    suppression des query

--- blocs/caracteristiques.php
+++ blocs/caracteristiques.php
@@ -18,62 +18,78 @@ $message="";
 ██║ ╚═╝ ██║╚██████╔╝██████╔╝██║██║██╗      ███████║╚██████╔╝███████╗
 ╚═╝     ╚═╝ ╚═════╝ ╚═════╝ ╚═╝╚═╝╚═╝      ╚══════╝ ╚══▀▀═╝ ╚══════╝
 */
-if ( isset($_POST["carac_valid"]) ) {
-
 /*  ╔═╗╔═╗╦═╗╔═╗╔═╗
     ║  ╠═╣╠╦╝╠═╣║
     ╚═╝╩ ╩╩╚═╩ ╩╚═╝ */
-    // Supprimer tous les compatibilités de cette entrée pour réinitialiser
-    $delcount = $dbh->exec("DELETE FROM carac WHERE carac_id=$i;");
+if (isset($_POST["carac_valid"])) {
+    $sql = "DELETE FROM carac WHERE carac_id = ?;";
+    $sth = $dbh->prepare($sql);
+    $delcount = $sth->execute([$i]);
 
-    // Ajout des caracteristiques
     if (isset($_POST["carac"])) {
-        $allc="";
+        $allc = [];
         foreach ($_POST["carac"] as $ck => $cd) {
-            $allc.= ($cd!="") ? "(\"$cd\",\"$i\",\"$ck\")," : "";
+            if ($cd != "") {
+                $allc[] = [$cd, $i, $ck];
+            }
         }
-        $allc=substr($allc, 0, -1); // suppression du dernier caractère
-	$modif_result = $dbh->query(str_replace("\"\"", "NULL","INSERT INTO carac (carac_valeur, carac_id, carac_caracteristique_id) VALUES $allc ;"));
-	$message.= (!isset($modif_result)) ? $message_error_modif : $message_success_modif;
 
+        $sql = "INSERT INTO carac (carac_valeur, carac_id, carac_caracteristique_id) VALUES (?, ?, ?);";
+        $sth = $dbh->prepare($sql);
+
+        $modif_result = true;
+        foreach ($allc as $values) {
+            if (!$sth->execute($values)) {
+                $modif_result = false;
+                break;
+            }
+        }
+
+        $sth->closeCursor();
+        $message .= (!$modif_result) ? $message_error_modif : $message_success_modif;
     }
 }
 
 
 
-if ( isset($_POST["new_carac_valid"]) ) {
+
+
 /*  ╔╗╔╔═╗╦ ╦╦  ╦╔═╗╦  ╦  ╔═╗  ╔═╗╔═╗╦═╗╔═╗╔═╗
     ║║║║ ║║ ║╚╗╔╝║╣ ║  ║  ║╣   ║  ╠═╣╠╦╝╠═╣║
     ╝╚╝╚═╝╚═╝ ╚╝ ╚═╝╩═╝╩═╝╚═╝  ╚═╝╩ ╩╩╚═╩ ╩╚═╝  */
-    // TODO : Vérifier que la catégorie n’existe pas déjà + que le symbole n’est pas déjà pris.
-    // Création d’une nouvelle caracteristique
-    $arr = array("nom_carac", "unite_carac", "symbole_carac");
+if (isset($_POST["new_carac_valid"])) {
+    $arr = ["nom_carac", "unite_carac", "symbole_carac"];
     foreach ($arr as &$value) {
-        $$value= isset($_POST[$value]) ? htmlentities(trim($_POST[$value])) : "" ;
+        $$value = isset($_POST[$value]) ? htmlentities(trim($_POST[$value])) : "";
     }
-    // Si au moins un champ de « Nouvelle caracteristique » est rempli :
-    if ( ($nom_carac!="")||($symbole_carac!="") ) {
-        // Si tous les champs sont remplis, on crée la nouvelle carac
-        if ( ($nom_carac!="")&&($symbole_carac!="") ) {
-            // Si la nouvelle carac existe déjà (nom ou symbôle)
-	    $sth = $dbh->query("SELECT COUNT(*) FROM caracteristiques WHERE nom_carac=\"$nom_carac\" OR symbole_carac=\"$symbole_carac\" ;");
-	    $count_carac = ($sth) ? $sth->fetchAll(PDO::FETCH_ASSOC) : FALSE ;
-	    if ($sth) $sth->closeCursor();
-	    $count_carac[0]["COUNT(*)"];
-	}
-       /* Récupère le nombre de lignes qui correspond à la requête SELECT */
-        if ($count_carac[0]["COUNT(*)"]!=0) $message.="<p class=\"error_message\">Nom ou symbôle déjà utilisé.</p>";
-        else {
-	    $sth = $dbh->query(str_replace("\"\"", "NULL","INSERT INTO caracteristiques (nom_carac, unite_carac, symbole_carac) VALUES (\"".$nom_carac."\", \"".$unite_carac."\", \"".$symbole_carac."\"); "));
-            if ( isset($sth) ) {
-                $message.="<p class=\"success_message\" id=\"disappear_delay\">La nouvelle caractéristique a été ajoutée.</p>";
-                $nom_carac=""; $unite_carac=""; $symbole_carac="";
+
+    if ($nom_carac != "" || $symbole_carac != "") {
+        if ($nom_carac != "" && $symbole_carac != "") {
+            $sql = "SELECT COUNT(*) FROM caracteristiques WHERE nom_carac = :nom_carac OR symbole_carac = :symbole_carac;";
+            $sth = $dbh->prepare($sql);
+            $sth->execute([':nom_carac' => $nom_carac, ':symbole_carac' => $symbole_carac]);
+            $count_carac = $sth->fetchColumn();
+            $sth->closeCursor();
+
+            if ($count_carac != 0) {
+                $message .= "<p class=\"error_message\">Nom ou symbôle déjà utilisé.</p>";
+            } else {
+                $sql = "INSERT INTO caracteristiques (nom_carac, unite_carac, symbole_carac) VALUES (:nom_carac, :unite_carac, :symbole_carac);";
+                $sth = $dbh->prepare($sql);
+                $modif_result = $sth->execute([':nom_carac' => $nom_carac, ':unite_carac' => $unite_carac, ':symbole_carac' => $symbole_carac]);
+                $sth->closeCursor();
+
+                if ($modif_result) {
+                    $message .= "<p class=\"success_message\" id=\"disappear_delay\">La nouvelle caractéristique a été ajoutée.</p>";
+                    $nom_carac = ""; $unite_carac = ""; $symbole_carac = "";
+                } else {
+                    $message .= "<p class=\"error_message\" id=\"disappear_delay\">Une erreur est survenue. La nouvelle caractéristique n’a pas été ajoutée.</p>";
+                }
             }
-            else { $message.="<p class=\"error_message\" id=\"disappear_delay\">Une erreur est survenue. La nouvelle caractéristique n’a pas été ajoutée.</p>"; }
         }
-    } // Sinon on indique un message d’erreur
-    //else $message.="<p class=\"error_message\">Nom et Symbôle sont des champs obligatoires.</p>";
+    }
 }
+
 
 
 /*
@@ -85,20 +101,26 @@ if ( isset($_POST["new_carac_valid"]) ) {
 ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝
 */
 
-// allcaracs
-$sth = $dbh->query("SELECT * FROM caracteristiques WHERE carac!=0 ORDER BY nom_carac ASC ;");
-$allcaracs = ($sth) ? $sth->fetchAll(PDO::FETCH_ASSOC) : FALSE ;
-if ($sth) $sth->closeCursor();
+// Sélection de toutes les caractéristiques
+$sql = "SELECT * FROM caracteristiques WHERE carac != 0 ORDER BY nom_carac ASC;";
+$sth = $dbh->prepare($sql);
+$sth->execute();
+$allcaracs = $sth->fetchAll(PDO::FETCH_ASSOC);
+$sth->closeCursor();
 
-// caracs de i
-$sth = $dbh->query("SELECT carac, carac_valeur FROM caracteristiques, carac, base WHERE carac_id=base_index AND carac_caracteristique_id=carac AND base_index='$i' AND carac!=0 ORDER BY base.base_index ASC, carac ASC ;");
-$caracs_i = ($sth) ? $sth->fetchAll(PDO::FETCH_ASSOC) : FALSE ;
-if ($sth) $sth->closeCursor();
+// Caractéristiques de l'élément 'i'
+$sql = "SELECT carac, carac_valeur FROM caracteristiques, carac, base WHERE carac_id = base_index AND carac_caracteristique_id = carac AND base_index = ? AND carac != 0 ORDER BY base.base_index ASC, carac ASC;";
+$sth = $dbh->prepare($sql);
+$sth->execute([$i]);
+$caracs_i = $sth->fetchAll(PDO::FETCH_ASSOC);
+$sth->closeCursor();
 
-// caracs_of_categorie
-$sth = $dbh->query("SELECT DISTINCT carac_caracteristique_id FROM carac, caracteristiques, base WHERE carac_id=base_index AND carac_caracteristique_id=carac AND categorie='".$data[0]["categorie"]."'");
-$car_of_cat = ($sth) ? $sth->fetchAll(PDO::FETCH_ASSOC) : FALSE ;
-if ($sth) $sth->closeCursor();
+// Caractéristiques de la catégorie
+$sql = "SELECT DISTINCT carac_caracteristique_id FROM carac, caracteristiques, base WHERE carac_id = base_index AND carac_caracteristique_id = carac AND categorie = ?;";
+$sth = $dbh->prepare($sql);
+$sth->execute([$data[0]["categorie"]]);
+$car_of_cat = $sth->fetchAll(PDO::FETCH_ASSOC);
+$sth->closeCursor();
 
 // Cas de création d’une nouvelle caracteristique, pour garder les champs remplis en cas d’erreur
 $arr = array("nom_carac", "unite_carac", "symbole_carac");
@@ -174,7 +196,7 @@ echo "<div id=\"bloc\" style=\"background:#daefc5; vertical-align:top;\">";
 	    echo "' name='carac[".$c["carac"]."]' type='text' id='carac[".$c["carac"]."]'>";
 	}
         echo "\">";
-        echo $c["nom_carac"];
+        echo $c["nom_carac"]." (".$c["symbole_carac"].")";
         echo "</option>";
     }
 
@@ -223,17 +245,17 @@ echo "</fieldset>";
     echo "Si la caractéristique n’est pas présente dans la liste ci-dessus…<br/>";
 
     echo "<label for=\"nom_carac\">Nom* :</label>\n";
-    echo "<input value=\"$nom_carac\" name=\"nom_carac\" type=\"text\"><br/>\n";
+    echo "<input value=\"$nom_carac\" name=\"nom_carac\" type=\"text\" required /><br/>\n";
 
     echo "<label for=\"unite_carac\">";
     echo "<abbr title=\"µm, %,… si oui/non, entrez « bool »\">Unité</abbr>";
     echo " :</label>\n";
-    echo "<input value=\"$unite_carac\" name=\"unite_carac\" type=\"text\"><br/>\n";
+    echo "<input value=\"$unite_carac\" name=\"unite_carac\" type=\"text\" /><br/>\n";
 
     echo "<label for=\"symbole_carac\">";
     echo "<abbr title=\"Plus court possible (ex: λ, ω₀, Tvisible,…)\">Symbôle* </abbr>";
     echo ":</label>\n";
-    echo "<input value=\"$symbole_carac\" name=\"symbole_carac\" type=\"text\"><br/>\n";
+    echo "<input value=\"$symbole_carac\" name=\"symbole_carac\" type=\"text\" required /><br/>\n";
 
 
     /*  ╔═╗╦ ╦╔╗ ╔╦╗╦╔╦╗
